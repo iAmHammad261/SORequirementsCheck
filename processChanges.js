236464c91ce7bb281f7f47475e5c99ab9749ee58
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2025-12-30 16:10:00 +0500

    added additional deal data

--- processChanges.js
+++ processChanges.js
@@ -43,7 +43,7 @@ export const processChanges = async (layoutDto, dealId) => {
         await markTheRequirementNotCompleted(layoutDto, "customerInfo_requirement02")
     }
 
-    if(contactData['UF_CRM_1766573650'] && contactData['UF_CRM_1767092446606']){
+    if(addtionalDealData['UF_CRM_1766573650'] && addtionalDealData['UF_CRM_1767092446606']){
         await markTheRequirementCompleted(layoutDto, "DP_IT_requirement03")
     }
     else {
