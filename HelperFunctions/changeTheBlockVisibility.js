ed38daff22f05015fcef83e740e81d29d201f08a
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2025-12-31 15:36:11 +0500

    fixed the visibility issue

--- HelperFunctions/changeTheBlockVisibility.js
+++ HelperFunctions/changeTheBlockVisibility.js
@@ -3,7 +3,7 @@ export const changeTheBlockVisibility = async (blockId, visibility) => {
     return new Promise((resolve, reject) => {
         BX24.placement.call('setLayoutItemState', {
             id: blockId,
-            visible: visibilty
+            visible: visibility
         }, (res) => {
             resolve();
         })});
