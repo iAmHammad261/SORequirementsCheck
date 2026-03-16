2b348106703e6627546afe65922e96ce5342cd2f
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-03-16 10:05:16 +0500

    fix: corrected the key in the object returned by the getPaymentDetail Function

--- HelperFunctions/getPaymentDetails.js
+++ HelperFunctions/getPaymentDetails.js
@@ -4,15 +4,15 @@ export const getPaymentDetails = async (dealId, dealData) => {
   const productRows = await getProductRows(dealId);
 
   const paymentDetails = {
-    productID: productRows.length > 0 ? productRows[0].PRODUCT_ID : null,
-    price: productRows.length > 0 ? productRows[0].PRICE_BRUTTO : null,
-    paymentPlan: dealData.UF_CRM_1767359953127,
-    paymentPlanUnits: dealData.UF_CRM_1767715497,
-    downpaymentPercent: dealData.UF_CRM_1766573650,
-    possessionPercent: dealData.UF_CRM_1767360946916,
-    paymentStartDates: dealData.UF_CRM_1767727123846,
-    modeOfPayment: dealData.UF_CRM_1767773115009,
-    chequePayOrderNo: dealData.UF_CRM_1767773157225,
+    PRODUCT_ID: productRows.length > 0 ? productRows[0].PRODUCT_ID : null,
+    PRICE: productRows.length > 0 ? productRows[0].PRICE_BRUTTO : null,
+    PAYMENT_PLAN: dealData.UF_CRM_1767359953127,
+    PAYMENT_PLAN_UNITS: dealData.UF_CRM_1767715497,
+    DOWN_PAYMENT_PERCENT: dealData.UF_CRM_1766573650,
+    POSSESSION_PERCENT: dealData.UF_CRM_1767360946916,
+    PAYMENT_START_DATE: dealData.UF_CRM_1767727123846,
+    MODE_OF_PAYMENT: dealData.UF_CRM_1767773115009,
+    CHEQUE_OR_PAY_ORDER_NUMBER: dealData.UF_CRM_1767773157225,
   };
   return paymentDetails;
 };
