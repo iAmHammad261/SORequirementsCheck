9d1002a9aa09b7f05733c4985ee0cfc10affe8e4
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-03-30 09:59:10 +0500

    feat: added lock and unlock when the api is called on primary button clicked

--- onButtonClick/onPrimaryButtonClick.js
+++ onButtonClick/onPrimaryButtonClick.js
@@ -1,43 +1,42 @@
 export const onButtonClick = async (buyerData, nomineeData, paymentDetails) => {
+  BX24.placement.call("lock");
 
-    const apiURL = 'https://bookingformtosalesorder.premierchoiceint.online/create-booking-form'
+  const API_URL =
+    "https://bookingformtosalesorder.premierchoiceint.online/create-booking-form";
 
+  console.log("Primary button was clicked. Executing onButtonClick handler.");
+  console.log("Received data in onButtonClick handler:");
+  console.log("Buyer Data:", buyerData);
+  console.log("Nominee Data:", nomineeData);
+  console.log("Payment Details:", paymentDetails);
 
+  const combinedData = {
+    buyerData,
+    nomineeData,
+    paymentDetails,
+  };
 
+  console.log("Combined Data to send to backend:", combinedData);
 
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(combinedData),
+    });
 
-    console.log("Primary button was clicked. Executing onButtonClick handler.");
-    console.log("Received data in onButtonClick handler:");
-    console.log("Buyer Data:", buyerData);
-    console.log("Nominee Data:", nomineeData);
-    console.log("Payment Details:", paymentDetails);
-
-
-    const combinedData = {
-        buyerData,
-        nomineeData,
-        paymentDetails
-    };
-
-    console.log("Combined Data to send to backend:", combinedData);
-
-    try {
-        const response = await fetch(apiURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(combinedData)
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        console.log("Response from backend:", result);
-    } catch (error) {
-        console.error("Error occurred while fetching data:", error);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-}
+    const result = await response.json();
+    console.log("Response from backend:", result);
+
+    BX24.placement.call("unlock");
+  } catch (error) {
+    console.error("Error occurred while fetching data:", error);
+    BX24.placement.call("unlock");
+  }
+};
